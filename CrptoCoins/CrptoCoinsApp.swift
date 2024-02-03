6c12fa161dea1b7aa41c8f4c49434ccc64050db8
dhanunjaykumar <mdhanunjay023@gmail.com>
2024-02-03 19:36:58 +0530

    Initial Commit

---
+++ CrptoCoins/CrptoCoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrptoCoinsApp.swift
+//  CrptoCoins
+//
+//  Created by Dhanunjay Kumar on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CrptoCoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
